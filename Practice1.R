d3d4e3f13d2076fdd29272b180baa2ada5292ebe
Aaron Kuin <adkuin@gmail.com>
2022-03-07 10:48:08 +0100

    Storyboard upload

--- Practice1.R
+++ Practice1.R
@@ -3,6 +3,7 @@ library(spotifyr)
 library(ggplot2)
 library(dplyr)
 library(plotly)
+library(compmus)
 
 OG <- get_playlist_audio_features("", "2WJJSs9WY3zaOPKHNKHAMK")
 Ghosts <- get_playlist_audio_features("", "1q8Tw6AvQ3K17zOhjZ8D0B")
@@ -10,8 +11,10 @@ OG
 Ghosts
 
 jointdataset <- rbind(OG, Ghosts)
-jointdataset
+
 ggplot(jointdataset, aes(x = track.popularity, y = playlist_name)) + geom_point()
+
+
 
 fig <- plot_ly(jointdataset, x=~tempo, y=~danceability, z=~energy, type="scatter3d", mode="markers", color =~playlist_name, size=1)
 fig
